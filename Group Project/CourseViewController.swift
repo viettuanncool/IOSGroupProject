83c1c3edf34ac69fb7fa5370d0e5c811dfa03c84
Tuan Nguyen <Tuan Nguyen>
2021-04-10 15:17:49 -0700

    first commit

---
+++ Group Project/CourseViewController.swift
@@ -0,0 +1,43 @@
+//
+//  CourseViewController.swift
+//  Group Project
+//
+//  Created by MacOS on 2021-04-04.
+//
+
+import UIKit
+
+class CourseViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet var tableView : UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell;
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        tableView.dataSource = self;
+        self.tableView.rowHeight = 120;
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
